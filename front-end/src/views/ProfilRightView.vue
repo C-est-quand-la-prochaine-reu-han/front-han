2bb74132fe8d518805e32df9153082ef8408b8f1
Ratsock <ratsockfofox@gmail.com>
2024-10-15 21:38:22 +0200

    Patch Friend Systéme

--- front-end/src/views/ProfilRightView.vue
+++ front-end/src/views/ProfilRightView.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-	import { get_all_users } from '@/jspong/main.js';
+	import { ref } from 'vue';
 	import { useAuthStore } from '../stores/auth.js';
 	import { get_me, get_user_by_id, request_refuse_friend, request_confirm_friend } from '/src/jspong/main.js';
 
@@ -19,36 +19,38 @@
 		if (me) {
 			friends_confirmed = me.friends_confirmed
 			for (let i = 0; i < friends_confirmed.length; i++) {
-				let new_friend = get_user_by_id(me.friends_confirmed[i]);
+				let new_friend = await get_user_by_id(me.friends_confirmed[i], token);
 				friend.push(new_friend);
 			}
 			friends_pending = me.friends_pending;
 			for (let i = 0; i < friends_pending.length; i++) {
-				new_pending = get_user_by_id(pending[i]);
+				let new_pending = await get_user_by_id(me.friends_pending[i], token);
 				pending.push(new_pending);
 			}
-			console.log(friend);
 		} else {
-			console.log('No user found');
+			pending = [];
+			friend = [];
+			console.log('ME not found');
 		}
 	} catch (error) {
-		console.log('No user found');
+		pending = [];
+		friend = [];
+		console.log('CATCH ERROR');
+		console.error(error);
 	}
-
-
 
 	async function rejectFriend(friend_id) {
 		console.log('refuse friend');
-		console.log(friend_id);
+		console.log(friend_id.pk);
 		console.log(token);
-		request_refuse_friend(friend_id, token);
+		request_refuse_friend(friend_id.pk, token);
 	}
 
 	async function acceptFriend(friend_id) {
 		console.log('accept friend');
-		console.log(friend_id);
+		console.log(friend_id.pk);
 		console.log(token);
-		request_confirm_friend(friend_id, token);
+		request_confirm_friend(friend_id.pk, token);
 	}
 
 </script>
@@ -60,10 +62,10 @@
 				<h2>Liste d'ami·e·s</h2>
 			</div>
 			<div class="friends-list">
-				<p v-if="friends_confirmed.length == 0" class="data-no-friend">Tu n'as pas d'ami·e LOOSER</p>
+				<p v-if="friends_confirmed.length == 0" class="data-no-friend">Tu n'as pas d'ami·e LOSER</p>
 				<div class="data-profil" v-for="friends in friend">
 					<img :src="friends.avatar" alt="Photo de profil">
-					<p>{{ friend.username }}</p>
+					<p>{{ friends.user_nick }}</p>
 				</div>
 			</div>
 		</div>
@@ -74,10 +76,11 @@
 			<div class="friends-list">
 				<p v-if="friends_pending.length == 0" class="data-no-friend">Tu n'as pas d'ami·e en attente</p>
 				<div class="data-profil" v-for="pendings in pending">
+					<!-- <p>{{ pendings }}</p> -->
 					<img :src="pendings.avatar" alt="Photo de profil">
-					<p>{{ pendings.username }}</p>
-					<button @click="acceptFriend(pending)">✔</button>
-					<button @click="rejectFriend(pending)">✘</button>
+					<p>{{ pendings.user_nick }}</p>
+					<button @click="acceptFriend(pendings)">✔</button>
+					<button @click="rejectFriend(pendings)">✘</button>
 				</div>
 			</div>
 		</div>
